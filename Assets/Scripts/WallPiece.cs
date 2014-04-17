da0c9dba581d525e6e422ee4121ca641e59802ec
Josh Lieberman <josh.lieberman92@gmail.com>
2014-04-17 12:43:31 -0400

    more work from yesterday, ready for demo

---
+++ Assets/Scripts/WallPiece.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallPiece : MonoBehaviour {
+
+	float destroyTime = 5.0f;
+	public static bool destroy = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		//
+		if(destroy) {
+			Destroy(gameObject, destroyTime);
+		}
+	}
+}
