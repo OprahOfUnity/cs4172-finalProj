df704a036d9eee6e9bc96476f5b93c2d846cf0bf
Josh Lieberman <josh.lieberman92@gmail.com>
2014-04-17 17:52:12 -0400

    have simple cannon shooting down walls game

---
+++ Assets/Scripts/DestroyCannonball.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyCannonball : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		// Destroy cannonball after 3 seconds
+		Destroy (gameObject, 3.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
