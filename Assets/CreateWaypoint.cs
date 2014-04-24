6d8b6de14b218628db64e42ed0494e3bdccab149
Will McAuliff <womcauliff@gmail.com>
2014-04-24 12:22:56 -0400

    started waypoint functionality

---
+++ Assets/CreateWaypoint.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateWaypoint : MonoBehaviour {
+
+	bool added;
+	// Use this for initialization
+	void Start () {
+		added = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (!added) {
+			GameObject footsoldiers = GameObject.FindGameObjectWithTag("footsoldiers");
+			if (footsoldiers) {
+				footsoldiers.GetComponent<SoldierMovement> ().addNewWayPoint ();
+			}
+			added = true;
+		}
+		
+	}
+}
