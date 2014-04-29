ebeb648d6484ff8e07660a905243eeda2f712436
Josh Lieberman <josh.lieberman92@gmail.com>
2014-04-29 09:32:44 -0400

    work on scene and cylinder target

---
+++ Assets/Scripts/ToggleOnTracking.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleOnTracking : MonoBehaviour {
+
+	CylinderTargetBehaviour ctb;
+
+	// Use this for initialization
+	void Start () {
+		ctb = this.GetComponent<CylinderTargetBehaviour>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (ctb.CurrentStatus == TrackableBehaviour.Status.UNKNOWN) {
+			transform.Find("Parent").gameObject.SetActive(false);
+		} else {
+			transform.Find("Parent").gameObject.SetActive(true);
+		}
+	
+	}
+}
