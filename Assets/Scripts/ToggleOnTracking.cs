4f75abe28483954379615f1e3c98d5dbcf6d5886
Josh Lieberman <josh.lieberman92@gmail.com>
2014-05-06 18:45:15 -0400

    GameObjects no longer fall off plane and (shouldn't) have any collision issues.
    I made a copy of the DefaultTrackableEventHandler script for the Cylinder Target and modified it to fit our needs: enabling and disabling the child gameobjects of the Cylinder Target.  If there are any issues let me know and I'll do my best to debug.

--- Assets/Scripts/ToggleOnTracking.cs
+++ Assets/Scripts/ToggleOnTracking.cs
@@ -7,25 +7,23 @@ public class ToggleOnTracking : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
-		ctb = this.GetComponent<CylinderTargetBehaviour>();
-		//		Debug.Log (ctb);
-		//		Debug.Log (ctb.CurrentStatus);
+//		ctb = this.GetComponent<CylinderTargetBehaviour>();
+//			Debug.Log (ctb);
+//			Debug.Log (ctb.CurrentStatus);
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+	
+//		if (ctb.CurrentStatus == TrackableBehaviour.Status.UNKNOWN) {
+//			Debug.Log("lost trackable, setting gameObject to false");
+//			// enable is kinematic here?
+//			transform.Find("CylinderTarget").gameObject.SetActive(false);
+//		} else {
+//			Debug.Log("gameObject should be active");
+//			transform.Find("CylinderTarget").gameObject.SetActive(true);
+//		}
 		
 	}
-	
-	//		if (ctb.CurrentStatus == TrackableBehaviour.Status.UNKNOWN) {
-	//			Debug.Log("lost trackable, setting gameObject to false");
-	//			// enable is kinematic here?
-	//			transform.Find("CylinderTarget").gameObject.SetActive(false);
-	//		} else {
-	//			Debug.Log("gameObject should be active");
-	//			transform.Find("CylinderTarget").gameObject.SetActive(true);
-	//		}
-	//	
-	//	}
 }
