5b2a3f6dacbd6becf8147cd642943976c1eac44a
Daniel Garzon <dg2796@columbia.edu>
2014-05-09 19:37:00 -0400

    Implemented Menu, and the toolbar now spawns soldiers, or cannons.

--- Assets/Scripts/RenderGUI.cs
+++ Assets/Scripts/RenderGUI.cs
@@ -13,7 +13,7 @@ public class RenderGUI : MonoBehaviour {
 		toolbar = GameObject.Find ("Toolbar");
 		handler = toolbar.GetComponent <VirtualButtonEventHandler>();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		hasSpawnedObject = handler.hasSpawnedObject;
@@ -25,17 +25,22 @@ public class RenderGUI : MonoBehaviour {
 		cameraStyle = new GUIStyle(GUI.skin.button);
 		cameraStyle.fontSize = 30;
 
-		if (GUI.Button (new Rect (10, 20, 280, 120), "Toggle moveSoldiers", cameraStyle)) {
+    if (GUI.Button (new Rect (Screen.width - 120, Screen.height - 120, 100, 100), "X", cameraStyle)) {
+      Application.LoadLevel(0);
+    }
+
+		if (GUI.Button (new Rect (20, 20, 280, 120), "Move Soldiers", cameraStyle)) {
 			GameObject footsoldiers = GameObject.FindGameObjectWithTag("footsoldiers");
 			footsoldiers.GetComponent<SoldierMovement> ().setMoveSoldiers ();
 		}
 
-		if (GUI.Button (new Rect (10, 150, 280, 120), "Toggle Waypoint Mode", cameraStyle)) {
+		if (GUI.Button (new Rect (20, 150, 280, 120), "Waypoint Mode", cameraStyle)) {
 			// waypoint toggle bool
 			SetWaypoint.toggleWaypointMode = !SetWaypoint.toggleWaypointMode;
 		}
+
 		if (hasSpawnedObject){
-			if (GUI.Button (new Rect (10, 280, 280, 120), "Reparent Toolbar Obj", cameraStyle)) {
+			if (GUI.Button (new Rect (20, 280, 280, 120), "Drop Object", cameraStyle)) {
 				SetSpawnedObject.toggleSetSpawnedObject = !SetSpawnedObject.toggleSetSpawnedObject;
 			}
 		}
---
+++ Assets/Scripts/TextEventHandler.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextEventHandler : MonoBehaviour {
+  public bool isQuitButton = false;
+  private bool hasTouched = false;
+
+  void Update () {
+    OnTouchDown();
+  }
+
+  void OnTouchDown () {
+    foreach (Touch touch in Input.touches) {
+      if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved) {
+        Ray ray = Camera.main.ScreenPointToRay(touch.position);
+        RaycastHit hit ;
+        if (Physics.Raycast (ray, out hit)) {
+          hit.transform.gameObject.SendMessage("OnMouseDown");
+          if (hit.transform.gameObject == this.gameObject && !isQuitButton){
+            this.renderer.material.color = Color.green;
+            hasTouched = !hasTouched;
+            Application.LoadLevel(1);
+          } else if (hit.transform.gameObject == this.gameObject && isQuitButton){
+            this.renderer.material.color = Color.red;
+            hasTouched = !hasTouched;
+            Application.Quit();
+          }
+        }
+      }
+    }
+  }
+}
